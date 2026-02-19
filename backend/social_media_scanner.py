9b90b542df1a2f33c5ea0ac9c3e9948351589fdf
Dikshant Nanwate <dikshantnanwate94@gmail.com>
2026-02-19 10:24:36 +0530

    Create backend/social_media_scanner.py file with RedditScanner and TwitterScanner classes

---
+++ backend/social_media_scanner.py
@@ -0,0 +1,15 @@
+class RedditScanner:
+    def __init__(self, username):
+        self.username = username
+
+    def scan(self):
+        # Logic to scan Reddit for user presence
+        pass
+
+class TwitterScanner:
+    def __init__(self, username):
+        self.username = username
+
+    def scan(self):
+        # Logic to scan Twitter for user presence
+        pass
